a50ec25dcc8830ffb63ddc17d36b88f1a5f6bd7c
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-07 14:53:37 -0500

    different approach

--- public/pIndex.js
+++
@@ -1,7 +0,0 @@
-const nodeBtn = document.getElementsByClassName('coll');
-
-// const testAlert = () => {
-//     alert('test alert')
-// }
-
-nodeBtn.addEventListener('click' , alert('balls'))
--- server/sIndex.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-
-app.use(express)
-
-app.listen(6006 , console.log('listening on 6006'))
