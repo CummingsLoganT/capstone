e22b4cb8593e6927d43a60d6372658e8613c5bc2
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-07 00:42:36 -0500

    initial commit

---
+++ public/pIndex.js
@@ -0,0 +1,7 @@
+const nodeBtn = document.getElementsByClassName('coll');
+
+// const testAlert = () => {
+//     alert('test alert')
+// }
+
+nodeBtn.addEventListener('click' , alert('balls'))
---
+++ server/sIndex.js
@@ -0,0 +1,5 @@
+const express = require('express')
+
+app.use(express)
+
+app.listen(6006 , console.log('listening on 6006'))
