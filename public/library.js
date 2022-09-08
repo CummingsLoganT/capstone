ccdbe7c3f65e0a49253e06946b737113be63e7ef
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-08 14:21:09 -0500

    lets try this again

---
+++ public/library.js
@@ -0,0 +1,24 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("mouseover", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+  coll[i].addEventListener("mouseout" , function () {
+    wait(10)
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
