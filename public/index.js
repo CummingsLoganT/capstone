f2f7b1ed9ccea82f27fcaca6af58f44cad21a1ce
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-09 15:49:05 -0500

    drag and drop function working properly. able to drag a copy, instead of moving an element. next is data sets and hierarchy. hopefully.

--- public/index.js
+++ public/index.js
@@ -7,26 +7,61 @@ function onDragStart (event) {
         .currentTarget
         .style
         .backgroundColor = 'yellow'
+
+    
 }
 
 function onDragOver (event) {
     event.preventDefault();
 }
 
-function onDrop (event) {
-    const id = event
-        .dataTransfer
-        .getData('text');
-        // console.log(event.dataTransfer.getData)
-        console.log(id)
 
-     const draggableElement = document.getElementById(id);
 
-     const dropzone = event.target;
 
-    dropzone.appendChild(draggableElement);
+// function onDrop (event) {
+// const id = event
+//     .dataTransfer
+//     .getData('text')
 
-    event
-        .dataTransfer
-        .clearData();
+// let nodeCopy = document.getElementById(id);
+
+
+// }
+
+
+/* 
+!!THIS IS OUR WORKING VERSION THAT DOESNT COPY 
+*/
+// function onDrop (event) {
+//     const id = event
+//         .dataTransfer
+//         .getData('text');
+//         // console.log(event.dataTransfer.getData)
+//         // console.log(id)
+
+//      const draggableElement = document.getElementById(id);
+
+//      const dropzone = event.target;
+
+//     dropzone.appendChild(draggableElement);
+
+//     event
+//         .dataTransfer
+//         .clearData();
+// }
+
+
+function onDrop(event) {
+    event.preventDefault();
+    var data=event.dataTransfer.getData("text")
+    console.log(data)
+
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    console.log(nodeCopy)
+    nodeCopy.id = "newId";
+    event.target.appendChild(nodeCopy);
+
 }
+
+
+// function so that grabbing from "sidebar" produces a child that can be iterated or organized, themed, blah blah blah
