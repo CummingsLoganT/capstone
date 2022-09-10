1a451e19237c318d55f2daf7cc71916c433d464a
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-09 21:18:16 -0500

    solid foundation(s)

--- public/index.js
+++ public/index.js
@@ -15,42 +15,6 @@ function onDragOver (event) {
     event.preventDefault();
 }
 
-
-
-
-// function onDrop (event) {
-// const id = event
-//     .dataTransfer
-//     .getData('text')
-
-// let nodeCopy = document.getElementById(id);
-
-
-// }
-
-
-/* 
-!!THIS IS OUR WORKING VERSION THAT DOESNT COPY 
-*/
-// function onDrop (event) {
-//     const id = event
-//         .dataTransfer
-//         .getData('text');
-//         // console.log(event.dataTransfer.getData)
-//         // console.log(id)
-
-//      const draggableElement = document.getElementById(id);
-
-//      const dropzone = event.target;
-
-//     dropzone.appendChild(draggableElement);
-
-//     event
-//         .dataTransfer
-//         .clearData();
-// }
-
-
 function onDrop(event) {
     event.preventDefault();
     var data=event.dataTransfer.getData("text")
@@ -62,6 +26,3 @@ function onDrop(event) {
     event.target.appendChild(nodeCopy);
 
 }
-
-
-// function so that grabbing from "sidebar" produces a child that can be iterated or organized, themed, blah blah blah
