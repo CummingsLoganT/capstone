b569be6a82496c2c6736087a8a8b0800a53536dc
CummingsLoganT <109982415+CummingsLoganT@users.noreply.github.com>
2022-09-11 15:39:24 -0500

    trying to make it look pretty

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+let dropCount = 0
+let hoverCounter = 1
+
+function stateChange () {
+    //potentially change the class and more?... 
+    // create global counters that will switch on and off different dropzones
+}
+
+//!drag related stuff\\
+
 function onDragStart (event) {
     event
         .dataTransfer
@@ -6,7 +16,7 @@ function onDragStart (event) {
     event
         .currentTarget
         .style
-        .backgroundColor = 'yellow'
+        .backgroundColor = 'rgb(37 , 109 , 133)'
 
     
 }
@@ -15,7 +25,9 @@ function onDragOver (event) {
     event.preventDefault();
 }
 
+
 function onDrop(event) {
+
     event.preventDefault();
     var data=event.dataTransfer.getData("text")
     console.log(data)
@@ -25,4 +37,25 @@ function onDrop(event) {
     nodeCopy.id = "newId";
     event.target.appendChild(nodeCopy);
 
+    dropCount++;
+    if (dropCount < 2){
+    stateChange();
+
+    }
 }
+//!drag//
+
+function showFirstNodes (res) {
+    document.querySelector(`${res}`).remove();
+}
+
+function checkCount() {
+    // wait(3)
+    if (dropCount === 0 && hoverCounter > 2) {
+        console.log(dropCount)
+        showFirstNodes('span');
+    }
+}
+
+
+checkCount();
